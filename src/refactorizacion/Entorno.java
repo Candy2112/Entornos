aa4cd9864ed1a9a3e50c71e5fbf10aa48d2f7e7a
Candy2112 <candela.r.espinosa@gmail.com>
2017-06-10 16:14:10 +0200

    Create Entorno.java

--- src/refactorizacion/Entorno.java
+++ src/refactorizacion/Entorno.java
@@ -3,15 +3,10 @@ package refactorizacion;
 public class Entorno 
 {
 	private int atrib;
-
-	public int getAtrib() 
+	private int atrib2;
+	
+	public int getAtrib()
 	{
 		return atrib;
-	}
-
-	public void setAtrib(int atrib) 
-	{
-		this.atrib = atrib;
-	}
-		
+	}	
 }
